0392c22a65ef87e0eb8c33e0c735087b17f18dd1
cedric <demuelenaere.cedric@gmail.com>
2026-03-09 12:50:36 +0100

    fixes + remove composable + add button to browse epub

--- the_android_application/android_studio_project/app/src/main/java/com/fluffycactus/wordperwordreader/MainActivity.kt
+++ the_android_application/android_studio_project/app/src/main/java/com/fluffycactus/wordperwordreader/MainActivity.kt
@@ -1,47 +1,39 @@
 package com.fluffycactus.wordperwordreader
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.fluffycactus.wordperwordreader.ui.theme.WordPerWordReaderTheme
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var selectedFileText: TextView
+
+    private val openEpubPicker = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+        if (uri != null) {
+            selectedFileText.text = getString(R.string.selected_epub, uri.lastPathSegment ?: uri.toString())
+            contentResolver.takePersistableUriPermission(
+                uri,
+                android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+        } else {
+            Toast.makeText(this, getString(R.string.epub_pick_cancelled), Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            WordPerWordReaderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val browseEpubButton = findViewById<Button>(R.id.browseEpubButton)
+        selectedFileText = findViewById(R.id.selectedFileText)
+
+        browseEpubButton.setOnClickListener {
+            openEpubPicker.launch(arrayOf("application/epub+zip"))
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WordPerWordReaderTheme {
-        Greeting("Android")
     }
 }
